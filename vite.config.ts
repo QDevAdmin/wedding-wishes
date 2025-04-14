b4ac3dcd2f7bee436f2a7fdccea7a0d46321b725
kiryu <nikonenko.kirill@wb.ru>
2025-04-14 03:33:16 +0300

    init

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "src",
+  build: {
+    outDir: '../dist',
+  },
   base: "wedding-wishes",
   plugins: [react()],
 })
